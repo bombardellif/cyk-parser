e62118f0243512d296cc80c9357eab2048dd06f9
william <william@william-Inspiron-N4050>
2013-05-20 00:29:23 -0300

    Upload de Arquivos Processar arquivo e interpretar gramática

--- Gramatica.php
+++ Gramatica.php
@@ -1,4 +1,10 @@
 <?php
+
+/**
+ * Necesita de Set para implementar os conjuntos
+ */
+require_once './Set.php';
+
 /**
  * Classe que representa a definição formal de uma gramatica.
  *
@@ -28,8 +34,8 @@ class Gramatica {
      * @var Palavra Variável inicial da gramática
      */
     private $inicial;
-    
-    public function __construct(Set $variaveis, Set $terminais, Set $producoes, Palavra $inicial) {
+        
+    public function __construct(Set $variaveis = null, Set $terminais = null, Set $producoes = null, Palavra $inicial = null) {
         $this->variaveis = $variaveis;
         $this->terminais = $terminais;
         $this->producoes = $producoes;
@@ -67,6 +73,155 @@ class Gramatica {
     public function setInicial(Palavra $inicial) {
         $this->inicial = $inicial;
     }
+    
+    /**
+     * Lê o arquivo fonte da gramática, inteepreta e configura o objeto de acordo com os dados lidos, supõe que a gramática é livre de contexto.
+     * Formato esperado do arquivo, dado que Ti são símbolos que representam os terminais (cada símbolo é uma string), 
+     * Vi são símbolos que representam as variáveis, S é a variável inicial, V são variáveis, W e Y são variáveis ou terminais
+     * Terminais
+     * { T1, T2, ..., Tn }      #O hash separa os comentários
+     * Variaveis
+     * { V1, V2, ..., Vn }      #Tudo que ocorre depois do # é desconsiderado
+     * Inicial
+     * { S }
+     * Regras
+     * { V > W , Y }
+     * ...
+     * 
+     * Note que cada regra é colocada em uma linha diferente, isto é, um "\r\n" ou "\n" separa cada regra
+     * Se existe uma regra X -> YZ na gramática, ela será representada por { X > Y , Z}, note que apenas um símbolo é permitido antes da seta ('>'), pois supõe que a gramática e livre do contexto. Não existe limite para símbolos após a seta.
+     * 
+     * @param String $nomeArquivo Caminho estático ao arquivo fonte da gramática
+     * @return void
+     * @throws LeituraArquivoGramaticaException
+     */
+    public function leDoArquivo($nomeArquivo){
+        
+        $conteudoArquivo = file($nomeArquivo);
+        if ($conteudoArquivo){
+            var_dump($conteudoArquivo);
+            
+            //Leitura dos Terminais
+            if ($this->isLinhaDef("Terminais", $conteudoArquivo[0])){
+                if ($this->isLinhaSet($conteudoArquivo[1])){
+                    $terminais = $this->criaSet($conteudoArquivo[1]);
+                    
+                    //Leitura das variáveis
+                    if($this->isLinhaDef("Variaveis", $conteudoArquivo[2])){
+                        if ($this->isLinhaSet($conteudoArquivo[3])){
+                            $variaveis = $this->criaSet($conteudoArquivo[3]);
+                            
+            var_dump($this->criaSet($conteudoArquivo[1]));
+            echo '<br />'; exit;
+                            
+                            /*//Leitura do Inicial
+                            if ($this->isLinhaDef("Inicial", $conteudoArquivo[4])){
+                                if ($this->isLinhaPalavra($conteudoArquivo[5])){
+                                    $inicial = $this->criaPalavra($conteudoArquivo[5]);
+                                    
+                                    //Leitura de Regras
+                                    if ($this->isLinhaDef("Regras", $conteudoArquivo[6])){
+                                        $i = 7;
+                                        $regras = new Set();
+                                        while($this->isLinhaRegra($conteudoArquivo[$i])){
+                                            $regras->union($this->criaSetRegra($conteudoArquivo[$i]));
+                                            $i++;
+                                        }
+                                    }
+                                }
+                            }*/
+                        }
+                    }
+                }
+            }
+            $this->terminais = $terminais;
+            $this->variaveis = $variaveis;
+            //$this->inicial = $inicial;
+            //$this->producoes = $regras;
+            
+        }else{
+            echo 'err'; exit;
+            //throw LeituraArquivoGramaticaException
+        }
+    }
+    
+    /**
+     * Verifica se a linha é de definição de escopo do arquivo de gramática. Por exemplo "Terminais   #Comentario" é um linha de definição.
+     * 
+     * @param string $stringDef String que define o escopo (p.e. "Terminais")
+     * @param string $linha Linha do arquivo a ser avalidada.
+     * @return boolean True se a linha é do modelo "$stringDef   # XXXXX", False caso contrário 
+     */
+    public function isLinhaDef($stringDef, $linha){
+        $regex = "/^$stringDef\s*((\s#).*)?/";
+        return preg_match($regex, $linha) == 1;
+    }
+    
+    /**
+     * Verifica se a linha é de definição de um conjunto. Por exemplo "{ A, B , C}    #Comentario" é definição de um conjunto.
+     * 
+     * @param string $linha Linha do arquivo a ser avalidada.
+     * @return boolean True se a linha é do modelo "{A, B , C,D, ...}   # XXXXX", False caso contrário 
+     */
+    public function isLinhaSet($linha){
+        $regex = "/^(({\s*})|({\s*((?![,{}\b\n\r\s#]).)+(\s*,\s*((?![,{}\b\n\r\s#]).)+)*\s*}))\s*((\s#).*)?$/";
+        return preg_match($regex, $linha) == 1;
+    }
+    
+    /**
+     * Verifica se a linha é de definição de um conjunto com um único elemento, ou uma única palavra. Por exemplo "{ S }   #Comentario" é definição de um conjunto com uma única palavra.
+     * 
+     * @param string $linha Linha do arquivo a ser avalidada.
+     * @return boolean True se a linha é do modelo "{S}   # XXXXX", False caso contrário 
+     */
+    public function isLinhaPalavra($linha){
+        $regex = "/^({\s*((?![,{}\b\n\r\s#]).)+\s*})\s*((\s#).*)?$/";
+        return preg_match($regex,$linha) == 1;
+    }
+    
+    /**
+     * Verifica se a linha é de definição de uma regra. Por exemplo "{ S > V, W }   #Comentario" é definição de uma regra.
+     * 
+     * @param string $linha Linha do arquivo a ser avalidada.
+     * @return boolean True se a linha é do modelo "{ S > V, W, ..., Z }  # XXXXX", False caso contrário 
+     */
+    public function isLinhaRegra($linha){
+        $regex = "/^{\s*((?![,{}\b\n\r\s#]).)+\s*>\s*((?![,{}\b\n\r\s#]).)+(\s*,\s*((?![,{}\b\n\r\s#]).)+)*\s}\s*((\s#).*)?$/";
+        return preg_match($regex,$linha) == 1;
+    }
+    
+    /**
+     * Supõe que a linha do parâmetro passou por isLinhaSet, isto é, é uma definição de conjunto. Sabendo disso 
+     * constói um Set com cada símbolo lido da linha. Note que cada símbolo é separado por vírgula e o conjunto é 
+     * delimitado pelas chaves "{" e "}"
+     * 
+     * @param string $linha Linha do arquivo a ser avalidada.
+     * @return Set Conjunto com os itens lidos da linha
+     */
+    public function criaSet($linha){
+        $set = new Set();
+        
+        //Pega só a parte até o fecha chaves, ignorando tudo após isto (espaços e comentários)
+        $linha = substr($linha, 0, strpos($linha, "}")+1);
+        var_dump($linha);
+        
+        //Pega o primeiro token (símbolo) do conjunto
+        $simbolo = trim(strtok($linha,",{}#"));
+        var_dump($simbolo);
+        //Se $linha = "{ A , B}", espera que $simbolo = "A"
+        
+        //Processa cada símbolo, adicionando ao conjunto final
+        while ($simbolo){
+            //Cria um conjunto com o símbolo e faz união ao conjunto final
+            $set->union(new Set(array($simbolo)));
+            //Próximo token, espera algo como "B", mesmo que B seja o último símbolo do cojunto
+            $simbolo = trim(strtok(",{}#"));
+            var_dump($simbolo);
+        }
+        
+        return $set;
+    }
+    
     
 }
 
--- controller.php
+++ controller.php
@@ -1,3 +1,60 @@
 <?php
-phpinfo();
+var_dump($_FILES);
+var_dump($_SESSION);
+
+ini_set('display_errors',1);
+
+require_once './Gramatica.php';
+
+define('APPLICATION_DIRECTORY', getcwd());
+
+//Verifica se já tem um arquivo na sessão
+if (isset($_SESSION) && isset($_SESSION['file']) && file_exists($_SESSION['file'])){
+    
+    //Interpreta o arquivo
+    $gramatica = new Gramatica();
+    $gramatica->leDoArquivo($_SESSION['file']['arquivoGramatica']);
+    
+}
+//Neste caso o usuário não enviou nada ainda, temos que processar tudo
+else{
+    $newFileName = APPLICATION_DIRECTORY . "/uploads/gramatica.txt";
+    if (moveArquivoGramatica($newFileName)){
+        
+        echo 'hehe<br />';
+        
+        //Interpreta o arquivo
+        $gramatica = new Gramatica();
+        $gramatica->leDoArquivo($newFileName);
+        
+        //Coloca na sessão o arquivo lido
+        $_SESSION['file']['arquivoGramatica'] = $newFileName;
+    }else{
+        echo 'erro';
+    }
+}
+
+
+
+function moveArquivoGramatica($newFileName){
+    //Verifica as informações do arquivo
+    if ($_FILES['file']['error'] == 0){
+        if ($_FILES['file']['type'] == "text/plain"){
+
+            //Move o arquivo para a pasta de uploads
+            
+            if (move_uploaded_file($_FILES['file']['tmp_name'], $newFileName)){
+                return true;
+            }else{
+                //throw Exception
+            }
+        }else{
+            //thor Exception
+        }
+    }else{
+        //throw Exception
+    }
+    return false;
+}
+
 ?>
